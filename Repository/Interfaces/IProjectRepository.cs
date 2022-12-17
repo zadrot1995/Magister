45e3af9d1f6d26719c3e6a0d18341139d155c78b
Zadrotdd <zadrotdd43@gmail.com>
2022-12-17 16:39:11 +0200

    add Project repository and fix name of ITeam repository

---
+++ Repository/Interfaces/IProjectRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface IProjectRepository : IDisposable
+    {
+        IEnumerable<Project> GetProjects();
+        Project GetProjectById(Guid id);
+        void InsertProject(Project project);
+        void DeleteProject(Project project);
+        void UpdateProject(Project project);
+        void Save();
+    }
+}
