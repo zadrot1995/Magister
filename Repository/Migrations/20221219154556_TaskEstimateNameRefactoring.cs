b100299de149916ed6869d83daddea9744a39d0b
Andriy Antonik <59703540+zadrot1995@users.noreply.github.com>
2022-12-19 17:47:17 +0200

    set changes to DB

---
+++ Repository/Migrations/20221219154556_TaskEstimateNameRefactoring.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class TaskEstimateNameRefactoring : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Entimate",
+                table: "Tasks",
+                newName: "Estimate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Estimate",
+                table: "Tasks",
+                newName: "Entimate");
+        }
+    }
+}
