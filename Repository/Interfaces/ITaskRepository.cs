eda9b4802e081fc13b82390ebaa459060546d2b6
Andriy Antonik <59703540+zadrot1995@users.noreply.github.com>
2022-12-19 16:01:36 +0200

    Adding task repository

---
+++ Repository/Interfaces/ITaskRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface ITaskRepository : IDisposable
+    {
+        IEnumerable<Domain.Models.Task> GetTasks();
+        Domain.Models.Task GetTaskById(Guid id);
+        void InsertTask(Domain.Models.Task task);
+        void DeleteTask(Domain.Models.Task task);
+        void UpdateTask(Domain.Models.Task task);
+        void Save();
+    }
+}
